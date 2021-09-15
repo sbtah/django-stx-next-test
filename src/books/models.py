38e50d5298201b5c08aa52bcc5e7a512f8e40017
Sbtha <shisastore@protonmail.com>
2021-09-15 18:10:13 +0200

    Added books model with list view.

---
+++ src/books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Book(models.Model):
+
+    name = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+    date_published = models.DateField()
+    isbn_number = models.CharField(max_length=120)
+    number_of_pages = models.PositiveIntegerField()
+    link_to_cover = models.URLField()
+    language = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f"{self.name} - {self.author}"
+
+    def get_absolute_url(self):
+
+        return reverse('books:book-detail', kwargs={
+            'pk': self.pk,
+        })
