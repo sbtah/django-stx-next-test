d0a09f302e13f6f0838beaf7016c9b27617d744d
Sbtha <shisastore@protonmail.com>
2021-09-15 23:13:27 +0200

    Added search bar for books. 1st part of test almost done ^^

--- src/books/views.py
+++ src/books/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Book
 from .forms import BookForm
+
+
+# Checks if query is valid.
+def is_valid_query(param):
+    return param != '' and param is not None
 
 
 # Lists all books in database.
@@ -9,7 +15,62 @@ def book_list(request):
 
     books = Book.objects.all()
 
+    # Getting data from form names.
+    title = request.GET.get('title',)
+    author = request.GET.get('author',)
+    date_from = request.GET.get('date_from',)
+    date_to = request.GET.get('date_to',)
+    isnb = request.GET.get('isbn',)
+    min_pages = request.GET.get('min-pages',)
+    max_pages = request.GET.get('max-pages',)
+    link = request.GET.get('link',)
+    language = request.GET.get('language',)
+
+    # This filter title.
+    if is_valid_query(title):
+
+        books = books.filter(title__icontains=title)
+
+    # This filter author.
+    elif is_valid_query(author):
+
+        books = books.filter(author__icontains=author)
+
+    # This filter for ISBN Number.
+    if is_valid_query(isnb):
+
+        books = books.filter(isbn_number=isnb)
+
+    # This filter for minimum number of pages.
+    if is_valid_query(min_pages):
+
+        books = books.filter(number_of_pages__gte=min_pages)
+
+    if is_valid_query(max_pages):
+
+        books = books.filter(number_of_pages__lte=max_pages)
+
+    # This filter for link to cover.
+    if is_valid_query(link):
+
+        books = books.filter(link_to_cover=link)
+
+    # This filter for language.
+    if is_valid_query(language):
+
+        books = books.filter(language=language)
+
+    # This filter a dates.
+    if is_valid_query(date_from):
+
+        books = books.filter(date_published__gte=date_from)
+
+    if is_valid_query(date_to):
+
+        books = books.filter(date_published__lte=date_to)
+
     return render(request, 'books/book_list.html', {
+
         'books': books,
 
     })
@@ -24,6 +85,7 @@ def book_create(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your book was added.')
             return HttpResponseRedirect('/books/?submitted=True')
     else:
         form = BookForm
@@ -47,7 +109,7 @@ def book_update(request, pk):
 
     if form.is_valid():
         form.save()
-
+        messages.success(request, 'Book updated.')
         return redirect('/books')
 
     context = {
