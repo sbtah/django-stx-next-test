94f2401b9a0f652c04bb3a666eab480da9387cff
Sbtha <shisastore@protonmail.com>
2021-09-19 16:26:52 +0200

    Secured some data in enviroment variable

---
+++ README.md
@@ -0,0 +1,10 @@
+# This is my recruitment test App. Even tho this went really poorly and probably I won't get this job, this project was really fun and humbling... 
+
+
+A list of things I learned:
+- Like 0.001 of Apis Topic,
+- Small part of Django Rest Framework,
+- How to get data from forms,
+- How to connect to API..., 
+- How to create enviroment variables,
+- and more....
--- src/main/settings.py
+++ src/main/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b++*g86a#s9=(n1y8l8e%_jf^!3bamzj9h5nu6yj8cjue+)16i'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = []
+
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
