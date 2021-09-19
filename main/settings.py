21cd221b51964f3ab76ccb3ef7806c66ede0fbf2
Sbtha <shisastore@protonmail.com>
2021-09-19 19:17:33 +0200

    Added django_heroku package

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATE_INPUT_FORMATS = ("%Y-%m-%d",)
+
+django_heroku.settings(locals())
